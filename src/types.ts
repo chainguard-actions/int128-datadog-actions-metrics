2fc382ba98c5eec68b6e8e3c319daecbbeb9b4c8
actionchain <actionchain@chainguard.dev>
2026-08-14 19:35:43 +0000

    Update int128/datadog-actions-metrics@v1.181.0

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import type { Endpoints } from '@octokit/types'
+
+export type WorkflowJobs =
+  Endpoints['GET /repos/{owner}/{repo}/actions/runs/{run_id}/attempts/{attempt_number}/jobs']['response']['data']['jobs']
+
+export type ListWorkflowRunsForRepoRateLimitResponse = Endpoints['GET /repos/{owner}/{repo}/actions/runs']['response']
+
+export type RateLimitResponse = Endpoints['GET /rate_limit']['response']
